126a030a2ef37bf20ac3c4a0c69b614e9fe99e8e
itisjoke <fecalia@gmail.com>
2011-11-05 03:31:12 +0400

    sql base

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dogs::Application.config.secret_token = 'b95043aa386a342b601942b85f292eb80a5b7c620794ca5a283d04903ff4a04471ee4b7b3000bfb0acdcb2c8f2bcf2edec4f44d32669e720fcde57b1f08b3233'
